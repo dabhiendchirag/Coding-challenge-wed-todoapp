355ccbb9f5a398f682fe8db5ab349943bfc21a6b
dabhiendchirag <dabhichirag2712@gmail.com>
2025-09-03 11:32:55 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
+auth_bp = Blueprint('auth', __name__) 
+
+USER_CREDENTIALS = {
+       'username': 'chirag',
+       'password': '12345'
+}
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		username = request.form.get('username')
+		password = request.form.get('password')
+		
+		if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
+			session['user'] = username
+			flash('Login Successful', 'success')
+			return redirect(url_for('tasks.view_tasks'))
+		else:
+			flash('Invalid username or password', 'danger')
+					
+	return render_template('login.html')
+
+@auth_bp.route('/logout')
+def logout():
+	session.pop('user', None)
+	flash('Logged out', 'Info')
+	return redirect(url_for('auth.login'))						
